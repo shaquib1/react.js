9047cbdf215463ea98168009890d0e6b1c4310e6
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-01 14:18:27 +0530

    Get input box value

---
+++ src/Components/GetInputBoxValue.jsx
@@ -0,0 +1,23 @@
+// In this component we learn how to get input box value means when we give input value how to get it ok
+
+import React, { useState } from "react";
+
+function GetInputDataBox() {
+
+    const [data, setData] = useState(null);
+
+    function getData(val) {
+        setData(val.target.value)
+    }
+
+    return (
+        <div>
+            <h1>hello get input box data </h1>
+            <h1>{data}</h1>
+            <input type="text" onChange={getData} />
+        </div>
+    )
+}
+
+
+export default GetInputDataBox;
