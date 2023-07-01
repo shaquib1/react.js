68a18d21d17edb70af76a0a4e28097fd4dc9a646
Md Shaquib Alam <shaquib475@gmail.com>
2023-07-01 19:24:39 +0530

    Function as a Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import Classcomponent from "./Classcomponent";
+import PropsFunction from "./Components/Function_as_props";
 
 function App() {
   //we can make a componet inside a componet for example
@@ -12,12 +13,18 @@ function App() {
       </div>
     );
   }
+
+  //make a function for props as a function for file funtion as a props
+  function test(){
+    alert("all correct function as a props")
+  }
   return (
     <div className="App">
       <h1>hello react</h1>
       <Classcomponent />
       <Tap />
       {Tap()}
+      <PropsFunction data={test} />
     </div>
   );
 }
---
+++ src/Components/Function_as_props.jsx
@@ -0,0 +1,18 @@
+
+// in this file we recived props which is a function in parent component
+
+// see its result upper of the page 
+//because this component imported in app.js and app.js is firstly imported in index.js
+
+function PropsFunction(props) {
+
+    return (
+        <div style={{ backgroundColor: "skyblue" }}>
+            <h1> funtion as a props </h1>
+            <button onClick={props.data}>click here </button>     this is output of function as a props component
+        </div>
+    )
+}
+
+
+export default PropsFunction;
